ebb32b128bd8de75f7a9ea8f096e041f851b2ce6
Tahir Khan <tahir_khan54321@hotmail.com>
2024-06-12 12:11:06 +0100

    Added base config in apps and settings

--- base/apps.py
+++ base/apps.py
@@ -1,1 +1,7 @@
 # This file is used to configure application-specific settings and behavior.
+
+from django.apps import AppConfig
+
+class BaseConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'base'
