11fb29afcb0b722da2e2788e615dde4bfe89e722
Tahir Khan <tahir_khan54321@hotmail.com>
2024-06-13 16:09:39 +0100

    Registered Movie in admin

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,8 @@
 # This file is used to customize the built-in Django admin interface, 
 # where you can register your models 
 # and configure how they should be displayed and edited.
+
+from django.contrib import admin
+from .models import Movie
+
+admin.site.register(Movie)
