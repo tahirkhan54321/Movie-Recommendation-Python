10179662e538cbb1e84269ab5bd2290ca23003e4
Tahir Khan <tahir_khan54321@hotmail.com>
2024-07-03 19:03:10 +0100

    Added settings, views, urls, html templates for user and login logout

--- base/views.py
+++ base/views.py
@@ -7,18 +7,15 @@ from django.shortcuts import render, redirect
 from .forms import MovieForm
 import re
 from .utils import initialize_tfidf, find_similar_movies
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from .forms import UserCreationForm
 
 # Global variables
 vectorizer = None
 tfidf = None
-
-# TBC - objective:
-# 1 - pull in the form submission
-# 2 - validate the form submission
-# 3 - find the right movie from the database to match the input (using cleaned_title), confirm this in the form (dropdown?)
-# 4 - use TFIDF to find most relevant movies using the movies.composite_string for both the input and the matrix
-# 5 - return the 10 most relevant movies
-# 6 - render them in a table
 
 def movie_search(request):
     form = MovieForm(request.POST or None)
@@ -37,6 +34,18 @@ def movie_search(request):
 
     return render(request, 'movie_search.html', {'form': form, 'similar_movies': similar_movies})
 
+class UserRegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)  # Log in the user immediately after registration
+        return response
+    
+class UserLoginView(LoginView):
+    template_name = 'registration/login.html'
 
 # helper functions
 def clean_title(movie_title):
