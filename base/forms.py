ad54c09b4f7fe083468d992d47d0d342ec449534
Tahir Khan <tahir_khan54321@hotmail.com>
2024-07-03 19:05:55 +0100

    Added forms.py user

--- base/forms.py
+++ base/forms.py
@@ -2,8 +2,15 @@
 
 from django.forms import ModelForm
 from .models import Movie
+from django import forms
+from django.contrib.auth.models import User
 
 class MovieForm(ModelForm):
     class Meta:
         model = Movie
         fields = ('title',)
+
+class UserCreationForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')  # Adjust fields as needed
