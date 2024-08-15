4d64240bee979d0bbf971a6fd683fd424b8d9b68
Tahir Khan <tahir_khan54321@hotmail.com>
2024-08-15 21:30:56 +0100

    Fixed form rendering for adding a review using crispy_bootstrap4

--- base/settings.py
+++ base/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base.apps.BaseConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
